14dfadfdfc40bed0b667fb2c50c2ce346a842380
Dogan Kirmizigul <dogankirmizigull@gmail.com>
2024-07-29 01:49:25 +0300

    conflict resolved.

--- src/models/HotelExperienceDto.ts
+++ src/models/HotelExperienceDto.ts
@@ -2,20 +2,10 @@ export class HotelExperienceDto {
   hotel_id: number;
   experience_id: number;
   rating: number;
-  hotel_name?: string;
-  experience_name?: string;
 
-  constructor(
-    hotel_id: number,
-    experience_id: number,
-    rating: number,
-    hotel_name?: string,
-    experience_name?: string,
-  ) {
+  constructor(hotel_id: number, experience_id: number, rating: number) {
     this.hotel_id = hotel_id;
     this.experience_id = experience_id;
     this.rating = rating;
-    this.hotel_name = hotel_name;
-    this.experience_name = experience_name;
   }
 }
